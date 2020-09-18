96e1588a36b26605629ab8a2836a8c2026fc8160
Christoph Hilty <christoph.hilty@spoud.io>
2020-09-18 18:28:54 +0200

    feat: add some cross entropy exploration

---
+++ chapters/plotting.py
@@ -0,0 +1,10 @@
+from fastai.vision.all import *
+
+# we borrow the plot_function from the fastbook library
+def plot_function(f, tx=None, ty=None, title=None, min=-2, max=2, figsize=(6,4)):
+    x = torch.linspace(min,max)
+    fig,ax = plt.subplots(figsize=figsize)
+    ax.plot(x,f(x))
+    if tx is not None: ax.set_xlabel(tx)
+    if ty is not None: ax.set_ylabel(ty)
+    if title is not None: ax.set_title(title)
